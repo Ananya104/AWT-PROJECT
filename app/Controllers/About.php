bee2e0ca71b26eab2776f7bab5f6b689c92ecdc1
Ananya104 <ananyam1702@gmail.com>
2022-11-04 13:44:00 -0400

    added files.

---
+++ app/Controllers/About.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controllers;
+
+class About extends BaseController
+{
+    
+    
+    public function index()
+    {
+        return view('About');
+        
+        
+    }
+    
+    
+}
+
